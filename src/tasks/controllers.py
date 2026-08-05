9d23250239c5a74c950639240f7efae15481fb98
AbhiBedre <bedreabhi1993@gmail.com>
2026-08-05 11:29:36 +0530

    Implement task management endpoints with CRUD operations

--- src/tasks/controllers.py
+++ src/tasks/controllers.py
@@ -1,2 +1,51 @@
-def create_task():
-    return {"message": "Task created successfully"}
+from src.tasks.dtos import TaskCreateDTO
+from sqlalchemy.orm import Session
+from src.tasks.models import TaskModel
+from fastapi import HTTPException
+
+def create_task(body: TaskCreateDTO, db: Session):
+    # print(f"Creating task with title: {body.model_dump()}")
+    data = body.model_dump()
+    new_task = TaskModel(**data)
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return {"message": "Task created successfully", "data": new_task}
+
+def get_tasks(db: Session):
+    tasks = db.query(TaskModel).all()
+    return {"message": "Tasks retrieved successfully", "data": tasks}
+
+def get_task_by_id(task_id: int, db: Session):
+    task = db.query(TaskModel).get(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return {"message": "Task retrieved successfully", "data": task}
+
+def update_task(task_id: int, body: TaskCreateDTO, db: Session):
+    task = db.query(TaskModel).get(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    # Update the task attributes using the model_dump method, user can update single or multiple attributes at once
+    for key, value in body.model_dump().items():
+        setattr(task, key, value)
+    # task.title = body.title
+    # task.description = body.description
+    # task.is_completed = body.is_completed
+
+    # db.add(task)
+    db.commit()
+    db.refresh(task)
+    return {"message": "Task updated successfully", "data": task}
+
+def delete_task(task_id: int, db: Session):
+    task = db.query(TaskModel).get(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db.delete(task)
+    db.commit()
+    return {"message": "Task deleted successfully"}
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -1,8 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.tasks import controllers
+from src.tasks.dtos import TaskCreateDTO
+from src.utils.db import get_db
 
 tasks_router = APIRouter(prefix="/tasks")
 
 @tasks_router.post("/create")
-def create_task():
-    return controllers.create_task()
+def create_task(body: TaskCreateDTO, db = Depends(get_db)):
+    return controllers.create_task(body, db)
+
+@tasks_router.get("/all_tasks")
+def get_tasks(db = Depends(get_db)):
+    return controllers.get_tasks(db)
+
+@tasks_router.get("/one_task/{task_id}")
+def get_task_by_id(task_id: int, db = Depends(get_db)):
+    return controllers.get_task_by_id(task_id, db)
+
+@tasks_router.put("/update_task/{task_id}")
+def update_task(task_id: int, body: TaskCreateDTO, db = Depends(get_db)):
+    return controllers.update_task(task_id, body, db)
+
+@tasks_router.delete("/delete_task/{task_id}")
+def delete_task(task_id: int, db = Depends(get_db)):
+    return controllers.delete_task(task_id, db)
